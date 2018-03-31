66ac6aa195fdb51cafb4c59c67e4c9da6814c32c
jokuskay <tafanasyev@gmail.com>
2018-03-31 22:01:12 +0700

    add sample

--- sample/src/main/kotlin/com/aafanasev/fonoapi/sample/KotlinSample.kt
+++ sample/src/main/kotlin/com/aafanasev/fonoapi/sample/KotlinSample.kt
@@ -2,6 +2,28 @@
 
 package com.aafanasev.fonoapi.sample
 
+import com.aafanasev.fonoapi.DeviceEntity
+import com.aafanasev.fonoapi.FonoApiFactory
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+// TODO: https://fonoapi.freshpixl.com/token/generate
+const val token = ""
+
 fun main(args: Array<String>) {
-    println("Hello World")
+
+    val fonoApi = FonoApiFactory().create()
+
+    fonoApi.getLatest(token, "google").enqueue(object : Callback<List<DeviceEntity>> {
+        override fun onResponse(call: Call<List<DeviceEntity>>, response: Response<List<DeviceEntity>>) {
+            response.body()?.forEach {
+                println(it.deviceName)
+            }
+        }
+
+        override fun onFailure(call: Call<List<DeviceEntity>>, t: Throwable) = t.printStackTrace()
+
+    })
+
 }
