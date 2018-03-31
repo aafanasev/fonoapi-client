2dd4c77770f3e075be3290f0d6ba7933fd2116b5
jokuskay <tafanasyev@gmail.com>
2018-03-31 22:46:18 +0700

    update sample project

--- sample/src/main/kotlin/com/aafanasev/fonoapi/sample/KotlinSample.kt
+++ sample/src/main/kotlin/com/aafanasev/fonoapi/sample/KotlinSample.kt
@@ -3,17 +3,25 @@
 package com.aafanasev.fonoapi.sample
 
 import com.aafanasev.fonoapi.DeviceEntity
-import com.aafanasev.fonoapi.FonoApiFactory
+import io.reactivex.functions.Consumer
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import com.aafanasev.fonoapi.retrofit.FonoApiFactory as RetrofitFonoApiFactory
+import com.aafanasev.fonoapi.rxjava.FonoApiFactory as ReactiveFonoApiFactory
 
 // TODO: https://fonoapi.freshpixl.com/token/generate
 const val token = ""
 
 fun main(args: Array<String>) {
+    showcaseRetrofit()
+    showcaseRxJava()
+}
 
-    val fonoApi = FonoApiFactory().create()
+fun showcaseRetrofit() {
+    println("-- showcaseRetrofit --")
+
+    val fonoApi = RetrofitFonoApiFactory().create()
 
     fonoApi.getLatest(token, "google").enqueue(object : Callback<List<DeviceEntity>> {
         override fun onResponse(call: Call<List<DeviceEntity>>, response: Response<List<DeviceEntity>>) {
@@ -24,6 +32,18 @@ fun main(args: Array<String>) {
 
         override fun onFailure(call: Call<List<DeviceEntity>>, t: Throwable) = t.printStackTrace()
 
+    })
+}
+
+fun showcaseRxJava() {
+    println("-- showcaseRxJava --")
+
+    val fonoApi = ReactiveFonoApiFactory().create()
+
+    fonoApi.getDevice(token, "pixel").subscribe(Consumer {
+        it.forEach {
+            println(it.deviceName)
+        }
     })
 
 }
