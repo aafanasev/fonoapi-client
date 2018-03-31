b055b08ddc16a9ba1b1f672581d77b5a2259d633
jokuskay <tafanasyev@gmail.com>
2018-03-31 23:00:32 +0700

    fix build

--- fonoapi-retrofit/src/main/kotlin/com/aafanasev/fonoapi/retrofit/FonoApiFactory.kt
+++ fonoapi-retrofit/src/main/kotlin/com/aafanasev/fonoapi/retrofit/FonoApiFactory.kt
@@ -1,5 +1,6 @@
 package com.aafanasev.fonoapi.retrofit
 
+import com.aafanasev.fonoapi.URL_BASE
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
--- fonoapi-rxjava/src/main/kotlin/com/aafanasev/fonoapi/rxjava/FonoApiFactory.kt
+++ fonoapi-rxjava/src/main/kotlin/com/aafanasev/fonoapi/rxjava/FonoApiFactory.kt
@@ -3,6 +3,7 @@ package com.aafanasev.fonoapi.rxjava
 import com.aafanasev.fonoapi.URL_BASE
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class FonoApiFactory {
@@ -11,6 +12,7 @@ class FonoApiFactory {
     fun create(okHttpClient: OkHttpClient? = null): FonoApiService = run {
         val retrofitBuilder = Retrofit.Builder()
                 .baseUrl(URL_BASE)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
 
         okHttpClient?.let {
