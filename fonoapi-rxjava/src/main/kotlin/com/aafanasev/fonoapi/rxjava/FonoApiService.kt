6ac70adaa0907faf84178cf89f772cbc0ad174ec
jokuskay <tafanasyev@gmail.com>
2018-03-31 23:42:56 +0700

    add javadoc

--- fonoapi-rxjava/src/main/kotlin/com/aafanasev/fonoapi/rxjava/FonoApiService.kt
+++ fonoapi-rxjava/src/main/kotlin/com/aafanasev/fonoapi/rxjava/FonoApiService.kt
@@ -7,6 +7,14 @@ import retrofit2.http.Query
 
 interface FonoApiService {
 
+    /**
+     * Search devices
+     *
+     * @param token access token (required)
+     * @param device search query, device name f.ex "k750i", "Nexus 4", etc (required)
+     * @param brand brand name f.ex "Apple", "Samsung", etc (optional)
+     * @param position position of device in result set (optional). If specified only 1 item will be returned
+     */
     @GET(URL_GET_DEVICE)
     fun getDevice(
             @Query(QUERY_TOKEN) token: String,
@@ -15,6 +23,13 @@ interface FonoApiService {
             @Query(QUERY_POSITION) position: Int? = null
     ): Single<List<DeviceEntity>>
 
+    /**
+     * Get the latest added devices
+     *
+     * @param token access token (required)
+     * @param brand search query, brand name f.ex "Apple", "Samsung", etc (required)
+     * @param limit limit the result count, max 100 (optional)
+     */
     @GET(URL_GET_LATEST)
     fun getLatest(
             @Query(QUERY_TOKEN) token: String,
